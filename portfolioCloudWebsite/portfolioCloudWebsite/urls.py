69dd50c2535b4b9b6330e0ea9213c0b1cab13294
Harshil Gandhi <hgandhi@Brads-Mac-Studio.local>
2023-12-19 11:00:44 -0700

    loaded static files in index.html

--- portfolioCloudWebsite/portfolioCloudWebsite/urls.py
+++ portfolioCloudWebsite/portfolioCloudWebsite/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from django.conf.urls.static import static
-from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('portfolio/', include('portfolio.urls'))
+]
