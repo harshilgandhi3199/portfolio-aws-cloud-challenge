562d0ae032673f338771066b39f8a62dfcc21b98
Harshil Gandhi <harshilgandhi3003@gamil.com>
2024-02-28 15:58:55 -0700

    created a separate sam-app

--- scripts/visitorCounter.js
+++
@@ -1,3 +0,0 @@
-fetch('https://rv1514d5nd.execute-api.us-east-1.amazonaws.com/Prod/counter')
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
